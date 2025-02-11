f35d3d06413c0f4df7be9bb7e3eb06f7c129a782
chowdy95 <chow_dianyi@yahoo.com.sg>
2025-02-11 23:07:44 +0800

    A priori feature selection, transformation with transcan
    Abortive attempt to use subject matter to pre-determine predictors (as the number of data points is too few for data driven feature selection). This was successful.
    However, also attempted to use transcan to transform data to optimise distribution of predictors and attempted to transform the dentists and dent_personnel predictors to minimise collinearity, but both transformations led to increased collinearity and poorer model performance

--- GBD.R
+++ GBD.R
@@ -4,10 +4,10 @@
 
 #renv related commands for my convenience
 #renv::init()
-#renv::install("rms","tidyverse")
+#renv::install("cowplot")
 #renv::status()
 renv::snapshot(type = "all")
-#renv::restore()
+renv::restore()
 
 
 # to restore()# to read csv into R
@@ -85,6 +85,9 @@ plot(summary(f))
 f3 <- ols(results ~ perm_caries + dentists + dent_personnel + dent_exppc, data = GBD, x = TRUE)
 f3
 
+#Testing variance inflation factors to detect collinearity
+vif(f3)
+
 #These 2 charts graphically show the effect size of the various predictors across the interquartile range
 ggplot(Predict(f3))
 plot(summary(f3))
@@ -98,58 +101,73 @@ qqnorm(r2)
 qqline(r2)
 
 
+
 ##A more systematic approach
 
 #1. Descriptive statistics for data
 
+d <- GBD %>% select(rowname,results,perio_prop_oral,perio_prev, decid_caries,perm_caries, 
+                    edent, diab_prev, diab_death, smoking_agest, dentists, dent_personnel,
+                    dent_pers_excl_dentists, gdp_usd, dent_exp, 
+                    dent_exppc,           #selecting only mean predictor values, excluding upper/lower CIs
+                    adv_coverage, coverage) %>% 
+  Hmisc::describe()
+
+d
+plot(d, bvspace=0.2)
+
+#All predictors have 0 missing values, except for dent_exp and dent_exppc with 2 missing values, 
+#adv_coverage with 85 missing values, and coverage with 92 missing values
+#dent_exp is very right skewed with a huge spike on the left
+#therefore may be less informative
+
+
 #2. Dimension reduction
 
-#There are 15 candidate predictors, but only 26 observations for perio expenditure, which 
-#suggests there is only sufficient data to support the use of 3 predictors
+#There are 16 candidate predictors, but only 26 observations for perio expenditure, which 
+#suggests there is only sufficient data to support the use of 3-4 predictors
 
-#Therefore we must eliminate 12 candidate predictors
-#This dimension reduction may be done through the use of variable clustering or PCA
-#Variable clustering was selected for better iinterpretabiliity, and also because it was
-#felt that the dataset was too small to support the complexity of PCA
+#Therefore we must eliminate at least 12 candidate predictors
 
-#We start by excluding predictors based on missingness and subject matter knowledge
-#perio-prop_oral is the prevalence of periodontitis as a proportion of the sum
-#of prevalence for all oral conditions. As such, we can exclude the prevalence
-#of other oral conditions
-#adv_coverage and coverage also have significant amounts of missing data and
-#therefore were excluded
-#dent_exp is also redundant as dent_exppc likely encodes all relevant information
+#We start by excluding predictors based on distribution, missingness and subject matter knowledge
 
-#This leaves us with 9 predictors, of which we must reduce to 3
-#We start with hierarchical clustering to assess highly correlated variables
-
-vc <- varclus(~perio_prop_oral + perio_prev + diab_prev + diab_death + 
-          smoking_agest + dentists + dent_personnel + gdp_usd +
-          dent_exppc, data = GBD)
-plot(vc)
-
-#Looking at dendrogram, we could select one predictor from each pair:
-#perio_prev/perio_prev_oral, dentists/dent_personnel, gdp_usd/dent_exppc
-#since they are strongly correlated with each other
-#Based on subject matter knowledge, we would choose perio_prev_oral, dent_personnel
-#and dent_exppc
-#This leaves us with 6 variables (perio_prev_oral, dent_personnel, dent_exppc, 
-#diab_death, diab_prev and smoking_agest)
-
-#The final selection of predictors is determined by subject matter knowledge.
-#There were 4 latent factors to be modeled by the predictors: treatment need (as modeled by
+#There were 3 latent factors to be modeled by the predictors: treatment need (as modeled by
 #perio_prop_oral), dental utilisation (as modeled by dent_exppc and dent_personnel) and
-#cost effectiveness of treatment (as modeled bydiab_death, diab_prev and smoking_agest) 
+#cost effectiveness of treatment (as modeled by diab_death, diab_prev and smoking_agest) 
 
 #However, prior knowledge suggests that the limiting factors are likely overall dental utilisation 
 #for periodontitis patients and treatment need, rather than cost effectiveness of treatment, given
 #the prevalence of untreated, partially treated, or undiagnosed periodontitis
+#Therefore, we exclude the 3 predictors associated with cost effectiveness (diab_death, diab_prev
+#and smoking_agest)
 
-#Therefore, the 3 predictors relating to cost effectiveness of treatment were eliminated from the
-#model, leaving the final 3 predictors:: dent_exppc, dent_personnel and perio_prop_oral
+#adv_coverage and coverage have significant amounts of missing data and
+#therefore were excluded
+
+#gdp and dent_exp had very right skewed distributions which might be expected to be less
+#informative, and this would not improve with data transformation as it is absolute dental
+#expenditure, not relative differences, that drive differences in perio expenditure
+
+#perio-prop_oral is the prevalence of periodontitis as a proportion of the sum
+#of prevalence for all oral conditions. As such, we can exclude the prevalence
+#of other oral conditions
+
+#population density of dentists is included in dent_personnel, likely leading to collinearity
+#Therefore a new predictor, dent_pers_excl_dentists was created by subtracting the pop density of 
+#dentists from dental personnel
+
+#This leaves us with the final set of 4 predictors: perio_prop_oral, dent_pers_excl_dentists
+#dent_personnel and dent_exppc
+
+#Variable clustering was then done to assess correlations between predictors
+vc <- varclus(~perio_prop_oral + dent_personnel + dent_pers_excl_dentists + dent_exppc, data = GBD)
+plot(vc)
+vc
+
+#Turns out that even after subtraction,dent_pers_excl_dentists and dent_personnel were very strongly correlated
 
 
-#3. Optimal transformation of 3 selected predictors to minimise collinearity, account for skewed 
+#3. Optimal transformation of 4 selected predictors to minimise collinearity, account for skewed 
 #data and for single imputation
 
 #Single imputation selected as for the predictors used, the number of missing values 
@@ -158,15 +176,13 @@ plot(vc)
 #These transformations only consider relationships between the variables, and do not consider the
 #relationship to the outcome variable, hence does not increase the risk of overfitting
 
-trans_model <- transcan(~perio_prop_oral + perio_prev + diab_prev + diab_death + 
-                          smoking_agest + dentists + dent_personnel + gdp_usd +
-                          dent_exppc, imputed = TRUE, 
-                      transformed = TRUE, pl= FALSE,
+trans_model <- transcan(~perio_prop_oral + dent_pers_excl_dentists + dent_personnel + dent_exppc, transformed = TRUE, pl= FALSE,
                       trantab = TRUE, data = GBD)
+summary(trans_model)
 
 #This plot would show the transformations for the
-#ggplot(trans_model, scale=TRUE) +
-#  theme(axis.text.x=element_text(size=6))
+ggplot(trans_model, scale=TRUE) +
+  theme(axis.text.x=element_text(size=6))
 
 GBD_trans <- predict(trans_model, GBD, type  = "transformed") %>%  #To extract transformed data from the trans_model
   as_tibble()                 
@@ -174,21 +190,30 @@ GBD_trans <- predict(trans_model, GBD, type  = "transformed") %>%  #To extract t
 GBD_trans <- GBD_trans %>% mutate(results = GBD$results,rowname = GBD$rowname) %>%
   relocate(rowname, results, .before = everything())      #To combine transformed data with the response variable and country names
 
+plot(describe(GBD_trans))
+#The distribution does not appear to have improved significantly
 
 #4. Linear regression
 
-f4 <- ols(results ~ perio_prop_oral + dent_personnel + dent_exppc, data = GBD_trans, x = TRUE)
+f4 <- ols(results ~ perio_prop_oral + dent_pers_excl_dentists + dent_personnel + dent_exppc, data = GBD_trans, x = TRUE)
 f4
+#r2 and adjusted R2 were substantially poorer than previous models
 
 #These 2 charts graphically show the effect size of the various predictors across the interquartile range
 ggplot(Predict(f4))
 plot(summary(f4))
+
+#Assessment of collinearity
 vif(f4)
+#Transformation with transcan did not help with transformation at all
 
 #This diagnostic test shows normally distributed residuals around the fitted values
 plot(f4, which = 1)
+#Residuals do not appear to be normally distributed
 
 #This Q-Q plot does not demonstrate any clear abnormalities
 r3 <- residuals(f4, type = "student")
 qqnorm(r3)
 qqline(r3)
+
+#Overall, the model using the transcan does not seem to have improved performance, nor did it minimise collinearity
