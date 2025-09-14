9456de23c715350927d49a7a5f3f4c347c1bde4d
chowdy95 <chow_dianyi@yahoo.com.sg>
2025-09-15 00:23:27 +0800

    Data Prep for 2025-2050 forecast
    2 new R files created: Data prep for forecast, and Monte Carlo model runner for 2025-2050 forecasts
    Data input from WHO and IHME (GBD) added to data files, cleaned up, joined to existing data files and prepped to be input for Monte Carlo model

---
+++ Data_prep_2050_forecast.R
@@ -0,0 +1,123 @@
+# ==============================================================================
+# Extracting data from IHME population forecast, perio and edentulism prevalence data
+# ==============================================================================
+
+library(tidyverse)
+library(countrycode)
+
+population_forecast <- read_csv("data/IHME_population_forecast.csv") %>%     # Data taken from IHME (https://ghdx.healthdata.org/record/ihme-data/global-population-forecasts-2017-2100)
+  filter(scenario == 0, year_id %in% c(2025, 2030, 2035, 2040, 2045, 2050)) %>%
+  select(starts_with(c("location", "year", "measure", "val","upper","lower"))) %>%
+  mutate(iso3c = countrycode(location_name, origin = "country.name", destination = "iso3c")) %>%   #matching with ISO code for country names
+  filter(!is.na(iso3c)) %>% # drop all locations not corresponding to countries (largely regions and super regions)
+  rename(Pop = val) %>%
+  mutate(Pop_sd = (upper-Pop)/2) %>%
+  select(!(upper:lower)) %>%
+  select(!(measure_id:measure_name)) # Noted that this dataset is missing Nauru, Palau, Saint Kitts and Nevis, Tuvalu and San Marino, which had to be taken from another dataset
+
+UN_population_forecast <- read_csv("data/UN_world_population_prospects.csv") %>%  # Data taken from UN World Population Prospects 2024 using the medium scenario (https://population.un.org/wpp/downloads?folder=Standard%20Projections&group=Most%20used)
+  filter(iso3c %in% c("NRU","TUV", "PLW", "KNA", "SMR", "MCO"))%>%
+  filter (Year %in% c(2025, 2030, 2035, 2040, 2045, 2050)) %>%
+  select(iso3c, Year, Pop) %>%
+  mutate (Pop = as.double(Pop)) %>%
+  mutate (Pop = Pop * 1000) %>%
+  rename(year_id = Year)
+
+population_forecast <- bind_rows(population_forecast, UN_population_forecast) %>%
+  select(!(location_id:location_name))
+
+perio_prevalence_forecast <- read_csv("data/GBD_prev_perio.csv") %>%     # Data taken from GBD forecasts
+  filter(year_id %in% c(2025, 2030, 2035, 2040, 2045, 2050)) %>%
+  filter(age_group_name == "All Ages", sex == "Both") %>%
+  mutate(iso3c = countrycode(location_name, origin = "country.name", destination = "iso3c")) %>%   #matching with ISO code for country names
+  filter(!is.na(iso3c)) %>%
+  rename(Perio_prev = mean) %>%
+  mutate(Perio_prev_sd = (upper-Perio_prev)/2) %>%
+  select(!(upper:lower)) %>%
+  select(!(sex:age_group_name)) %>%
+  select(!(location_id:location_name))
+
+
+edent_prevalence_forecast <- read_csv("data/GBD_prev_edent.csv") %>%     # Data taken from GBD forecasts
+  filter(year_id %in% c(2025, 2030, 2035, 2040, 2045, 2050)) %>%
+  filter(age_group_name == "All Ages", sex == "Both") %>%
+  mutate(iso3c = countrycode(location_name, origin = "country.name", destination = "iso3c")) %>%   #matching with ISO code for country names
+  filter(!is.na(iso3c)) %>%
+  rename(Edent_prev = mean) %>%
+  mutate(Edent_prev_sd = (upper-Edent_prev)/2) %>%
+  select(!(upper:lower)) %>%
+  select(!(sex:age_group_name)) %>%
+  select(!(location_id:location_name))
+
+
+joined_list <- list(perio_prevalence_forecast, edent_prevalence_forecast, population_forecast)
+
+joined_forecast <- reduce(joined_list, left_join, by = c("year_id", "iso3c"))
+
+
+# ==============================================================================
+# Creating datasets for each year, joining by iso3c, and adjusting procedure costs for medical inflation
+# ==============================================================================
+
+existing_input <- read_csv("data/combined_country_input.csv")
+
+existing_input_stripped <- existing_input %>%
+  select(!(Pop:Perio_prev_sd)) %>%
+  mutate(Country = ifelse(Country == "Micronesia",             # Micronesia is the only country that is not captured by the countrycodes package
+                          "Micronesia (Federated States of)", 
+                          Country),
+         iso3c = countrycode(Country, origin = "country.name", destination = "iso3c"))
+
+# Prepare 2025 file, joining by iso3c
+
+combined_country_input_2025 <-  joined_forecast %>%
+  filter(year_id ==2025) %>%
+  select(!(year_id)) %>%
+  right_join(existing_input_stripped) %>%
+  write_csv("data/combined_country_inputs_2025.csv")
+
+# Regarding estimate for medical inflation, would refer to Dunn et al. (2016) (https://pmc.ncbi.nlm.nih.gov/articles/PMC5785315/)
+# Based on CPI-U vs the Medical Care component of CPI, the medical care component has been broadly in line with real inflation
+# In fact in 2021 and 2022 it was outstripped by overall CPI by 3-4%
+# Therefore, no further adjustments were made for medical inflation
+
+# Prepare 2030 file
+
+combined_country_input_2030 <-  joined_forecast %>%
+  filter(year_id ==2030) %>%
+  select(!(year_id)) %>%
+  right_join(existing_input_stripped) %>%
+  write_csv("data/combined_country_inputs_2030.csv")
+
+# Prepare 2035 file
+
+combined_country_input_2035 <-  joined_forecast %>%
+  filter(year_id ==2035) %>%
+  select(!(year_id)) %>%
+  right_join(existing_input_stripped)  %>%
+  write_csv("data/combined_country_inputs_2035.csv")
+
+# Prepare 2040 file
+
+combined_country_input_2040 <-  joined_forecast %>%
+  filter(year_id ==2040) %>%
+  select(!(year_id)) %>%
+  right_join(existing_input_stripped) %>%
+  write_csv("data/combined_country_inputs_2040.csv")
+
+# Prepare 2045 file
+
+combined_country_input_2045 <-  joined_forecast %>%
+  filter(year_id ==2045) %>%
+  select(!(year_id)) %>%
+  right_join(existing_input_stripped) %>%
+  write_csv("data/combined_country_inputs_2045.csv")
+
+
+# Prepare 2050 file
+
+combined_country_input_2050 <-  joined_forecast %>%
+  filter(year_id ==2050) %>%
+  select(!(year_id)) %>%
+  right_join(existing_input_stripped) %>%
+  write_csv("data/combined_country_inputs_2050.csv")
---
+++ Monte Carlo Model Cleaned 2050 Extrapolation.R
@@ -0,0 +1,51 @@
+# ==============================================================================
+# Cost Model Function — Optimised Version with Cleanup
+# ==============================================================================
+
+source("./99_functions.R")
+
+# ============================================================================
+# Load packages 
+# ============================================================================
+
+library(tidyverse)
+library(MCMCpack)
+
+# ==============================================================================
+# Calling the function
+# ==============================================================================
+
+# Example file paths (adjust if needed)
+countries_path <- "./data/combined_country_input.csv"
+procedure_lookup_high_path <- "./data/procedure_lookup.csv"
+procedure_lookup_mid_path <- "./data/procedure_lookup.csv"
+#  procedure_lookup_high_path <- "./data/procedure_lookup_increased_maintenance.csv"   #for various scenarios
+#  procedure_lookup_mid_path  <- "./data/procedure_lookup_increased_maintenance.csv"
+procedure_lookup_low_path <- "./data/procedure_lookup_low_scenario.csv"
+#  procedure_lookup_low_path  <- "./data/procedure_lookup_low_scenario_increased_maintenance.csv"
+tx_split_high_path <- "./data/tx_split.csv"
+tx_split_mid_path <- "./data/tx_split_mid_scenario.csv"
+tx_split_low_path <- "./data/tx_split_low_scenario.csv"
+severity_split_path <- "./data/severity_split.csv"
+
+# Where to write outputs
+output_dir <- "outputs"
+
+n_sims = 3000
+
+# Call the new scenario runner
+run_cost_model_scenarios(
+  countries_path = countries_path,
+  procedure_lookup_high_path = procedure_lookup_high_path,
+  procedure_lookup_mid_path = procedure_lookup_mid_path,
+  procedure_lookup_low_path = procedure_lookup_low_path,
+  tx_split_high_path = tx_split_high_path,
+  tx_split_mid_path = tx_split_mid_path,
+  tx_split_low_path = tx_split_low_path,
+  severity_split_path = severity_split_path,
+  n_sims = n_sims,
+  output_dir = output_dir,
+  scenarios = c("low", "mid", "high"), # same as before
+  slice_size = 999
+)
+
